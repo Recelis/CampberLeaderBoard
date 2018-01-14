260337aee24f71ae30a774599bce3eafdbfe1ece
JackyRecelis <jackymlui@live.com>
2018-01-14 18:29:50 +1000

    github button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import Table from './components/Table'
+import github from "./img/github.png";
 
 class App extends Component {
+
+  openWindow(){
+    window.open("https://github.com/Recelis/CamperLeaderBoard",'_blank');
+  }
   render() {
     return (
       <div>
         <Table />
-        <p className="Copyright">Created by Jacky Lui using REACTJS</p>
+        <p className="Copyright">Created by Jacky Lui using REACTJS
+          <span><button onClick = {()=>this.openWindow()}className = "github"><img className = "github" src = {github}></img></button></span>
+        </p>
       </div>
     );
   }
